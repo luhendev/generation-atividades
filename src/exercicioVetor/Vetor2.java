88c8a3c51c4cc2b132728935c320ece61f02c35e
Lucas Henrique <lucas.silva01052@gmail.com>
2024-11-06 08:29:10 -0300

    Att 06/11

---
+++ src/exercicioVetor/Vetor2.java
@@ -0,0 +1,40 @@
+package exercicioVetor;
+
+import java.util.Scanner;
+
+public class Vetor2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int[] vetor = new int[10];
+		int soma = 0;
+		double media;
+
+		for (int i = 0; i < 10; i++) {
+			System.out.printf("Vetor: ", +(i + 1) + ": ");
+			vetor[i] = scanner.nextInt();
+			soma += vetor[i];
+		}
+
+		System.out.print("\nElementos nos índices ímpares: ");
+		for (int i = 1; i < 10; i += 2) {
+			System.out.print(vetor[i] + " ");
+		}
+
+		System.out.print("\nElementos pares: ");
+		for (int num : vetor) {
+			if (num % 2 == 0) {
+				System.out.print(num + " ");
+			}
+		}
+
+		System.out.println("\nSoma: " + soma);
+
+		media = soma / 10.0;
+		System.out.printf("Média: %.2f%n", media);
+
+		scanner.close();
+	}
+
+}
